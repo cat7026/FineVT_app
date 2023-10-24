4c2b7cce0b7899bceb1cf3e05441ae077c852808
cat7026 <114451916+cat7026@users.noreply.github.com>
2023-10-24 15:52:53 +0900

    toast message update

--- app/src/main/java/com/example/finevttt/MainActivity.kt
+++ app/src/main/java/com/example/finevttt/MainActivity.kt
@@ -4,18 +4,18 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // activity_main 레이아웃을 설정
 
-        setContentView(R.layout.activity_main)
         // 검색 버튼을 찾습니다.
         val searchButton = findViewById<Button>(R.id.search_btn)
-
         // 검색 버튼에 클릭 리스너를 추가합니다.
         searchButton.setOnClickListener { // 클릭 시 실행할 코드를 작성합니다.
             val intent = Intent(this@MainActivity, SubActivity::class.java)
--- app/src/main/java/com/example/finevttt/SubActivity.kt
+++ app/src/main/java/com/example/finevttt/SubActivity.kt
@@ -1,11 +1,24 @@
 package com.example.finevttt
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+
+        // 이미지 클릭 시 토스트 메시지
+        val imageView = findViewById<ImageView>(R.id.imgShowToast)
+        imageView.setOnClickListener {
+            // 이미지 클릭 시 실행할 코드
+            showToast("이미지를 클릭했습니다.")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
